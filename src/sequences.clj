e52b0f0e2e5c83764b354a737dfce8f5d27df33f
Vernon Grant <vernon@ruppell.io>
2022-12-06 23:09:50 +0700

    Removed duplicate entries

--- src/sequences.clj
+++ src/sequences.clj
@@ -17,3 +17,11 @@
   (println "Hello World"))
 
 (hello)
+
+(map :a 1 :b 2)
+;(type [])
+;clojure.lang.PersistentVector
+
+(type peek)
+
+(def my-vec [0 1 2 3 4 5])
---
+++ test/sequences_test.clj
@@ -0,0 +1,3 @@
+(ns sequences-test
+  (:require [clojure.test :refer :all])
+  (:require [sequences :refer :all]))
