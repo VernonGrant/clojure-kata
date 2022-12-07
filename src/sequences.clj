8777319263e3e43f1127fdcc5c915ac61470c5c3
Vernon Grant <vernon@ruppell.io>
2022-12-07 10:23:12 +0700

    Adds additional problem

--- src/sequences.clj
+++ src/sequences.clj
@@ -1,27 +1,26 @@
 (ns sequences)
 
-;; Heading
+;; Splitting a vector into two parts.
 ;; -----------------------------------------------------------------------------
 ;;
 ;; Description:
-;; This is a simple description of the problem.
+;;
+;; Given an equally lengthened vector, split it into its two equal parts and
+;; return a new vector containing these parts.
 ;;
 ;; My Solution:
-;; - Step one.
-;; - Step two.
 ;;
+;; - Calculate the vectors half.
+;; - Use subvec to take each half.
 
-(defn hello
-  "Example documentation string."
-  []
-  (println "Hello World"))
+(def numbers (into [] (range 13)))
 
-(hello)
-
-(map :a 1 :b 2)
-;(type [])
-;clojure.lang.PersistentVector
-
-(type peek)
-
-(def my-vec [0 1 2 3 4 5])
+;; First solution.
+;;
+;; Notes: I was thinking of using the partition function, but that will return a
+;; sequence. I'm still trying to comprehend when and how to use sequences with
+;; collections interchangeably.
+(let [half (int (/ (count numbers) 2))
+      part-a (subvec numbers 0 half)
+      part-b (subvec numbers half (count numbers))]
+  [part-a part-b])
